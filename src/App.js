353b01647be918bef6c660907b6c094473cbd2e1
BoViKa <127208807+BoViKa@users.noreply.github.com>
2023-07-09 20:27:46 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from "./Components/Header/header"
+
+
+export default function App() {
+
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
