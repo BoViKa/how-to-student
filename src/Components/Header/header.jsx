1ba6dbbc3e7e5bb3e6dda332186c5c8b53b57dd6
Viggo Halvarsson Skoog <127208807+BoViKa@users.noreply.github.com>
2023-07-26 19:29:38 +0200

    Continued editing Header

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
-import styles from "./header.css";
+import styles from "./header.scss";
 
 const Header = ({ children, transparent }) => {
     const [mainMenuExpanded, setMainMenuExpanded] = useState(false);
