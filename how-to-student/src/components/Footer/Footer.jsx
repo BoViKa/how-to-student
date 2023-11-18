f1b835ee835d27c3de9495670a5df272748a4d4d
Viggo Halvarsson Skoog <viggohs@kth.se>
2023-11-18 02:03:22 +0100

    New start
    Added components header, footer

---
+++ how-to-student/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css'
+
+function Footer() {
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} How To Student</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ how-to-student/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+
+function Header() {
+    return(
+        <header>
+            <h1>How To Student</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Les go</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
