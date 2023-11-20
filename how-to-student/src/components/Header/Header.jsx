3d033a98555686ffae8799784fa107ddc20f86d9
Viggo Halvarsson Skoog <127208807+BoViKa@users.noreply.github.com>
2023-11-20 18:32:17 +0100

    Added tailwind
    removed css files and added tailwind plugin

--- how-to-student/src/components/Header/Header.jsx
+++ how-to-student/src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
-import './Header.css'
+
 
 function Header() {
     return(
-        <header>
-            <h1>How To Student</h1>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Les go</a></li>
-                </ul>
+        <header className="min-h-3/10 bg-sky-400">
+            <nav className="flex sm:justify-center space-x-4">
+            {[
+                ['Home', '/'],
+                ['Team', '/#'],
+                ['Projects', '/#'],
+                ['Reports', '/#'],
+            ].map(([title, url]) => (
+                <a href={url} className="px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">{title}</a>
+            ))}
             </nav>
         </header>
     );
